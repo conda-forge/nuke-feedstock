7452265860a221fb794aed93502d1e4db5d51b7c
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-02 18:38:05 -0800

    enable rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+ln -sf ${DOTNET_ROOT}/dotnet ${PREFIX}/bin
 
 # Build package with dotnet publish
 rm -rf global.json
@@ -15,6 +16,7 @@ tee ${PREFIX}/bin/nuke << EOF
 #!/bin/sh
 exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/nuke/Nuke.GlobalTool.dll "\$@"
 EOF
+chmod +x ${PREFIX}/bin/nuke
 
 tee ${PREFIX}/bin/nuke.cmd << EOF
 call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\nuke\Nuke.GlobalTool.dll %*
